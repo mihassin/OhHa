5a0e70bd035fad7ab8d0aee5a42fe2e14baac107
Marko I Hassinen <mihassin@melkki.cs.helsinki.fi>
2012-09-12 15:40:51 +0300

    ruutu luokka

---
+++ miinaharava/src/miinaharava/Ruutu.java
@@ -0,0 +1,38 @@
+package miinaharava;
+
+public class Ruutu {
+    
+    private boolean miinoitettuRuutu;
+    private boolean avattuRuutu;
+    private boolean liputettuRuutu;
+    
+    public Ruutu() {
+        miinoitettuRuutu = false;
+        avattuRuutu = false;
+        liputettuRuutu = false;
+    }
+    
+    public boolean onkoMiina() {
+        return miinoitettuRuutu;
+    }
+    
+    public boolean onkoAvattu() {
+        return avattuRuutu;
+    }
+    
+    public boolean onkoLippu() {
+        return liputettuRuutu;
+    }
+    
+    public void asetaMiina(){
+        miinoitettuRuutu = true;
+    }
+    
+    public void asetaAvattu() {
+        avattuRuutu = true;
+    }
+    
+    public void asetaLippu() {
+        liputettuRuutu = true;
+    }
+}
